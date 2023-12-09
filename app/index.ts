5e409574539fd2caf195ba96682c3952bb03f381
Stefan <decimelli@ibm.com>
2023-12-08 23:54:09 -0500

    add messaging

--- app/index.ts
+++ app/index.ts
@@ -1,17 +1,33 @@
-import express, { Express, Request, Response } from "express";
-import pino from "pino";
+import { Express, Request, Response } from "express";
+const express = require("express");
 
-const logger = pino({ level: 'info' });
+import pino from "pino";
+import * as http from "http";
+import {Server} from "socket.io";
+
+const logger = pino({ level: 'info', timestamp: pino.stdTimeFunctions.isoTime });
 const app: Express = express();
+const server = http.createServer(app);
+const io = new Server(server);
 const port = 3000;
 
 app.get("/", (req: Request, res: Response) => {
-  res.setHeader("Content-Type", "application/json")
-  res.send({
-    message: "Hello, world!"
+  res.setHeader("Content-Type", "text/html")
+  res.sendFile(__dirname + "/index.html");
+});
+
+io.on('connection', (socket) => {
+  const client = socket.handshake.headers["user-agent"];
+  const childLogger = logger.child({ client });
+  childLogger.info("user connected");
+  socket.on('user', (msg) => {
+    childLogger.info(msg);
+  });
+  socket.on('disconnect', () => {
+    childLogger.info("user disconnected");
   });
 });
 
-app.listen(port, () => {
-  logger.info(`Server is running at http://localhost:${port}`);
+server.listen(3000, () => {
+  logger.info(`listening on http://localhost:${port}`);
 });
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Bilateral Browser Communication
+This app is a sandbox for testing bilateral messages being sent to the server from the browser. 
+I made this to learn two things:
+ 1. Socket.io, and how to go about starting a socket project
+ 2. Using only TypeScript for a Express.js server, and starting it from the ground up, no frameworks
